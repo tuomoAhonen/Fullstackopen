d56efdfdac87be8bee78cc1d834c831b91a96666
tuomoAhonen <35593887+tuomoAhonen@users.noreply.github.com>
2023-02-03 12:59:28 +0200

    Add files via upload

--- osa2/puhelinluettelo/src/services/personService.js
+++ osa2/puhelinluettelo/src/services/personService.js
@@ -3,23 +3,17 @@ import axios from 'axios';
 const dburl = 'http://localhost:3001/persons';
 
 const getAll = async () => 
-  await axios
-    .get(dburl)
-    .then(response => response.data);
+  await axios.get(dburl).then(response => response.data);
 
 const createPerson = async (newPerson) => 
-  await axios
-    .post(dburl, newPerson);
+  await axios.post(dburl, newPerson);
 
 const updatePerson = async (id, updatedPerson) => 
-  await axios
-    .put(`${dburl}/${id}`, updatedPerson);
+  await axios.put(`${dburl}/${id}`, updatedPerson);
 
 const deletePerson = async (id) => 
-  await axios
-    .delete(`${dburl}/${id}`);
+  await axios.delete(`${dburl}/${id}`);
     
-
 // eslint-disable-next-line
 export default {
   getAll,
